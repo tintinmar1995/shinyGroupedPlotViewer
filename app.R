1ac3e73f28de378ec0bfc604c9dc2963dd1f0a5f
Martin Masson <martin.masson@datastorm.fr>
2023-09-27 18:00:36 +0200

    ENH: More robust way to deal with regex (order between groups and chart)

--- app.R
+++ app.R
@@ -13,10 +13,11 @@ library(shiny)
 ############################################
 #### A RENSEIGNER AVANT DE LANCER L'APP ####
 location = '..'
+#regex = "(?<group>[A-Z0-9 \\.]{5,5}-[0-9]{3,3})_(?<chart>[A-Z_]*).png"
+regex = "(?<chart>balance)_(?<group>[A-Z0-9\\-_ \\.]*).png"
 ############################################
 ############################################
 
-regex = "(?<group>[A-Z0-9 \\.]{5,5}-[0-9]{3,3})_(?<chart>[A-Z_]*).png"
 shiny::addResourcePath('charts', location)
 
 ui <- fluidPage(
@@ -46,23 +47,37 @@ server <- function(input, output, session) {
     # charts = stringr::str_match(files, regex)
     parsed = stringr::str_match(files, input$regex)
     charts(parsed)
-    updateSelectInput(
-      session=session, inputId='groups',
-      choices=as.character(na.omit(unique(parsed[, "group"]))))
+    
+    groups = as.character(na.omit(unique(parsed[, "group"])))
+    if(length(groups) == 0){
+      showModal(modalDialog(
+        title = "Problème de regex ?",
+        "Aucune instance de graphique trouvé !"))
+    } else {
+      updateSelectInput(
+        session=session, inputId='groups',
+        choices=groups)  
+    }
+    
     chartnames = as.character(na.omit(unique(parsed[, "chart"])))
-    updateSelectInput(
-      session=session, inputId='charts', 
-      choices=chartnames, selected = chartnames)
+    if(length(chartnames) == 0){
+      showModal(modalDialog(
+        title = "Problème de regex ?",
+        "Aucun type de graphique trouvé !"))
+    } else {
+      updateSelectInput(
+        session=session, inputId='charts', 
+        choices=chartnames, selected = chartnames)
+    }
   })
 
   output$charts <- renderUI({
-    
     if(nchar(input$groups) == 0) return(NULL)
     
-    sel = (charts()[, 2] == input$groups) & (charts()[, 3] %in% input$charts)
+    sel = (charts()[, 'group'] == input$groups) & (charts()[, 'chart'] %in% input$charts)
     
-    sel_fname = charts()[sel,1]
-    sel_cname = charts()[sel,3]
+    sel_fname = charts()[sel, 1]
+    sel_cname = charts()[sel, 'chart']
     
     sel = as.character(na.omit(sel_fname))
     names(sel) = sel_cname
@@ -72,7 +87,6 @@ server <- function(input, output, session) {
     imgs = lapply(sel, function(x) tags$img(src=x, width=input$width))
     return(do.call(fluidRow, imgs))
   })
-  
 }
 
 # Run the application 
